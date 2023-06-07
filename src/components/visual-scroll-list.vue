632613d0106cffb87e3628471ef70566544b4d46
akeng <war3hack@qq.com>
2023-06-07 22:00:17 +0800

    feat: 🎸 不定高虚拟列表实现

--- src/components/visual-scroll-list.vue
+++ src/components/visual-scroll-list.vue
@@ -4,16 +4,24 @@
     :style="{ height, width }"
     ref="scrollContainerRef"
   >
-    <div class="phantomContent" :style="{ height: wrapperHeight + 'px' }">
+    <div
+      class="phantomContent"
+      :style="{ position: 'relative', height: phantomContentHeight + 'px' }"
+    >
       <div
         class="scroll-wrapper"
         :style="{
+          position: 'absolute',
           height: wrapperHeight + 'px',
           transform: wrapperScrollTransform,
         }"
         ref="scrollWrapperRef"
       >
-        <div v-for="(item, index) in list" :key="item[itemKey]">
+        <div
+          v-for="(item, index) in list"
+          :key="item[itemKey]"
+          :id="'item-' + (startIndex + index)"
+        >
           <slot name="item" v-bind="{ item, index }"> </slot>
         </div>
       </div>
@@ -22,7 +30,8 @@
 </template>
 <script setup lang="ts">
 import { PropType, computed, nextTick, onMounted, onUnmounted, ref } from "vue";
-// const emit = defineEmits(["scroll-end"]);
+import { throttle, binarySearch, CompareResult } from "../util";
+const emit = defineEmits(["scroll-end"]);
 const props = defineProps({
   list: {
     type: Array as PropType<any[]>,
@@ -40,6 +49,9 @@ const props = defineProps({
     type: String,
     default: "100%",
   },
+  estimateHeight: {
+    type: Number,
+  },
   isGetting: {
     type: Boolean,
     default: false,
@@ -49,7 +61,13 @@ const props = defineProps({
     default: 28,
   },
 });
-
+type HeightCache = {
+  index: number;
+  height: number;
+  top: number;
+  bottom: number;
+  dValue: number;
+};
 const rowHeight = ref(0);
 const scrollTop = ref(0);
 const startIndex = ref(0);
@@ -58,12 +76,10 @@ const viewCount = ref(1);
 const scrollContainerRef = ref<HTMLDivElement>();
 const scrollWrapperRef = ref<HTMLDivElement>();
 const containerRect = ref<DOMRect>();
+const heightCahce = ref<HeightCache[]>([]);
 const wrapperScrollTransform = computed(() => {
-  return `translate3d(0, ${
-    scrollTop.value -
-    (scrollTop.value % rowHeight.value) -
-    Math.min(startIndex.value, props.bufferSize) * rowHeight.value
-  }px, 0)`;
+  // 滑动高度 - 当前被截断 - 缓冲区
+  return `translate3d(0, ${startIndex.value >= 1 ? heightCahce.value[startIndex.value]?.bottom : 0}px, 1px)`;
 });
 const list = computed(() => {
   const start = startIndex.value;
@@ -71,19 +87,31 @@ const list = computed(() => {
   return props.list.slice(start, end);
 });
 const wrapperHeight = computed(() => {
-  return props.list.length * rowHeight.value;
+  return list.value.length * rowHeight.value;
+});
+const phantomContentHeight = computed(() => {
+  const len = props.list.length;
+  if (heightCahce.value.length) return heightCahce.value[len - 1].bottom;
+  return len * rowHeight.value;
 });
 onMounted(async () => {
   await nextTick();
   getViewData();
   initScrollEvent();
+  nextTick(() => {
+    initCacheHeight();
+    updateHeightCache();
+  });
 });
 onUnmounted(() => {
   removeScrollEvent();
 });
 function getViewData() {
   if (!scrollContainerRef.value) return;
-  rowHeight.value = getRowHeight();
+  // 如果传入了对元素的预估高度，则直接使用此高度不进行计算
+  rowHeight.value = props.estimateHeight
+    ? props.estimateHeight
+    : getRowHeight();
   containerRect.value = scrollContainerRef.value.getBoundingClientRect();
   viewCount.value = Math.ceil(containerRect.value.height / rowHeight.value);
   console.log(containerRect.value);
@@ -101,13 +129,16 @@ function getRowHeight() {
 
 function calcRenderIndex() {
   const total = props.list.length;
-  const currentStartIndex = Math.floor(scrollTop.value / rowHeight.value);
+  const currentStartIndex = getStartIndex(scrollTop.value);
   if (currentStartIndex !== startIndex.value) {
     startIndex.value = Math.max(currentStartIndex - props.bufferSize, 0);
     endIndex.value = Math.min(
       currentStartIndex + viewCount.value + props.bufferSize,
       total - 1
     );
+    if(endIndex.value >= total -1) {
+      scolleEnd()
+    }
   }
   console.log(startIndex.value, endIndex.value);
 }
@@ -115,6 +146,9 @@ function handleOnScroll() {
   const wrapper = scrollContainerRef.value;
   calcRenderIndex();
   scrollTop.value = wrapper?.scrollTop || 0;
+  nextTick(() => {
+    tupdateHeightCache();
+  });
 }
 function initScrollEvent() {
   const wrapper = scrollContainerRef.value;
@@ -124,13 +158,87 @@ function removeScrollEvent() {
   const wrapper = scrollContainerRef.value;
   wrapper?.removeEventListener("scroll", handleOnScroll);
 }
-function getTransform() {
-  return `translate3d(0, ${
-    scrollTop.value -
-    (scrollTop.value % rowHeight.value) -
-    Math.min(startIndex.value, props.bufferSize) * rowHeight.value
-  })`;
+function initCacheHeight() {
+  const estimateHeight = rowHeight.value;
+  for (let i = 0; i < props.list.length; ++i) {
+    heightCahce.value[i] = {
+      index: i,
+      height: estimateHeight,
+      top: i * estimateHeight,
+      bottom: (i + 1) * estimateHeight,
+      dValue: 0,
+    };
+  }
 }
+const tupdateHeightCache = throttle(updateHeightCache, 100);
+function updateHeightCache() {
+  const wrapper = scrollWrapperRef.value;
+  if (!wrapper) return;
+  const childrens = Array.from(wrapper.children);
+  const heightCacheVal = heightCahce.value;
+  const start = childrens[0];
+  childrens.forEach((node) => {
+    if (!node) return;
+    const rect = node.getBoundingClientRect();
+    const { height } = rect;
+    const index = Number(node.id.split("-")[1]);
+    const oldHeight = heightCacheVal[index].height;
+    const dValue = oldHeight - height;
+
+    if (dValue) {
+      heightCacheVal[index].bottom -= dValue;
+      heightCacheVal[index].height = height;
+      heightCacheVal[index].dValue = dValue;
+    }
+  });
+  let startIdx = 0;
+  if (start) {
+    startIdx = Number(start.id.split("-")[1]);
+  }
+  const cacheLen = heightCacheVal.length;
+  let cumulativeDiffHeight = heightCacheVal[startIdx].dValue;
+  heightCacheVal[startIdx].dValue = 0;
+  for (let i = startIdx + 1; i < cacheLen; ++i) {
+    const item = heightCacheVal[i];
+    heightCacheVal[i].top = heightCacheVal[i - 1].bottom;
+    heightCacheVal[i].bottom = heightCacheVal[i].bottom - cumulativeDiffHeight;
+    if (item.dValue !== 0) {
+      cumulativeDiffHeight += item.dValue;
+      item.dValue = 0;
+    }
+  }
+  console.log("update", heightCacheVal);
+}
+function getStartIndex(st = 0) {
+  let idx = binarySearch<HeightCache, number>(
+    heightCahce.value,
+    st,
+    (currentValue, targetValue) => {
+      const currentCompareValue = currentValue.bottom;
+      if (currentCompareValue === targetValue) {
+        return CompareResult.eq;
+      } else if (currentCompareValue < targetValue) {
+        return CompareResult.lt;
+      }
+      return CompareResult.gt;
+    }
+  );
+  const targetItem = heightCahce.value[idx];
+  if (targetItem.bottom < st) {
+    idx += 1;
+  }
+  return idx;
+}
+const scolleEnd = throttle(()=>{
+  emit('scroll-end')
+}, 100)
+// function getTransform() {
+//   return `translate3d(0, ${
+//     scrollTop.value -
+//     (scrollTop.value % rowHeight.value) -
+//     Math.min(startIndex.value, props.bufferSize) * rowHeight.value
+//   })`;
+// }
 // function getItemScrollStyle(index: number) {
 //   if (rowHeight.value <= 0) return undefined;
 //   return {
--- src/util/index.ts
+++ src/util/index.ts
@@ -1,6 +1,6 @@
 export function throttle(fn: Function, wait: number) {
     let timeout: ReturnType<typeof setTimeout>;
-    return ()=> {
+    return () => {
         let args = arguments;
         clearTimeout(timeout);
         timeout = setTimeout(function () {
@@ -8,4 +8,29 @@ export function throttle(fn: Function, wait: number) {
         }, wait);
     };
 }
+export enum CompareResult {
+    eq = 1,
+    lt,
+    gt,
+}
+export function binarySearch<T, VT>(list: T[], value: VT, compareFunc: (current: T, value: VT) => CompareResult) {
+    let start = 0;
+    let end = list.length - 1;
+    let tempIndex = null;
 
+    while (start <= end) {
+        tempIndex = Math.floor((start + end) / 2);
+        const midValue = list[tempIndex];
+        const compareRes: CompareResult = compareFunc(midValue, value);
+
+        if (compareRes === CompareResult.eq) {
+            return tempIndex;
+        }
+        if (compareRes === CompareResult.lt) {
+            start = tempIndex + 1;
+        } else if (compareRes === CompareResult.gt) {
+            end = tempIndex - 1;
+        }
+    }
+    return tempIndex as number;
+}
